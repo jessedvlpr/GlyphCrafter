340d9273b4a8fef7e1bb7bd2ab49ce6e9d38a554
Jesse <grovesjesse@gmail.com>
2023-09-30 22:55:45 -0400

    moved cookie related code into cookies.js

--- js/index.js
+++ js/index.js
@@ -35,34 +35,3 @@ function resetColours() {
     changeColour('bg');
     changeColour('text');
 }
-
-function setCookie(name, value, days) {
-    const d = new Date();
-    d.setTime(d.getTime() + (days * 24 * 60 * 60 * 1000));
-    let expires = "expires=" + d.toUTCString();
-    document.cookie = name + "=" + value + ";" + expires + ";path=/";
-}
-
-function getCookie(name) {
-    let n = name + "=";
-    let ca = decodeURIComponent(document.cookie).split(';');
-    for (let i = 0; i < ca.length; i++) {
-        let c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(n) == 0) {
-            return c.substring(n.length, c.length);
-        }
-    }
-    return "";
-}
-
-function checkCookie(name) {
-    let cookie = getCookie(name);
-    if (cookie != "" && cookie != null) {
-        return true;
-    } else {
-        return false;
-    }
-}
