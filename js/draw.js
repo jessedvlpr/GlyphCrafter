cc08836841d35a1abbf2dfcaeb85b0875d9cb6f8
Jesse <grovesjesse@gmail.com>
2023-10-05 13:25:59 -0400

    some pretty ugly lookin shape generation

--- js/draw.js
+++ js/draw.js
@@ -1,6 +1,6 @@
 var mouseX = 0, mouseY = 0;
 
-document.onmousemove = function(e) {
+document.onmousemove = function (e) {
     mouseX = e.clientX;
     mouseY = e.clientY;
 }
@@ -12,16 +12,16 @@ const viewBox = {
 }
 
 var svgRect = svg.getClientRects()[0];
-var svgCenter = {x: ((svgRect.width/2) * (viewBox.width/svgRect.width)).toFixed(3), y: ((svgRect.height/2) * (viewBox.height/svgRect.height)).toFixed(3)};
+var svgCenter = { x: ((svgRect.width / 2) * (viewBox.width / svgRect.width)).toFixed(3), y: ((svgRect.height / 2) * (viewBox.height / svgRect.height)).toFixed(3) };
 var sx, sy, ex, ey, x, y;
 var clicked = false;
 
 
-document.body.onresize = function () { 
+document.body.onresize = function () {
     refresh();
 }
 
-document.body.onscroll = function () { 
+document.body.onscroll = function () {
     refresh();
 }
 
@@ -32,7 +32,7 @@ svg.onmousedown = startLine = function (e) {
     if (clicked) return;
     sx = x = ((mouseX - svgRect.left) * (viewBox.width / svgRect.width)).toFixed(3);
     sy = y = ((mouseY - svgRect.top) * (viewBox.height / svgRect.height)).toFixed(3);
-    if ((x - svgCenter.x)**2 + (y - svgCenter.y)**2 > 500**2) return;
+    if ((x - svgCenter.x) ** 2 + (y - svgCenter.y) ** 2 > 500 ** 2) return;
     clicked = true;
 
     undoList = [];
@@ -55,14 +55,29 @@ svg.onmouseup = endLine = function (e) {
     ex = ((mouseX - svgRect.left) * (viewBox.width / svgRect.width)).toFixed(3);
     ey = ((mouseY - svgRect.top) * (viewBox.height / svgRect.height)).toFixed(3);
 
-    if((sx - ex)**2 + (sy - ey)**2 < 150**2){
+    if ((sx - ex) ** 2 + (sy - ey) ** 2 < 150 ** 2) {
         line.setAttribute("points", line.getAttribute("points") + ` ${sx},${sy}`);
     }
-    
-    let points = cullPoints(line.getAttribute("points").split(" ")
-    .map((el) => el.split(",").map((el2) => parseFloat(el2))));
 
-    line.setAttribute("points", points.join(" "));
+    let points = cullPoints(line.getAttribute("points").split(" ")
+        .map((el) => el.split(",").map((el2) => parseFloat(el2))));
+
+    let leastX = points[0][0];
+    let leastY = points[0][1];
+    let mostX = points[0][0];
+    let mostY = points[0][1];
+    for (let i = 0; i < points.length; i++) {
+        for (let j = 0; j < points.length; j++) {
+            if (mostX < points[j][0])
+                mostX = points[j][0];
+            if (leastX > points[j][0])
+                leastX = points[j][0];
+            if (mostY < points[j][1])
+                mostY = points[j][1];
+            if (leastY > points[j][1])
+                leastY = points[j][1];
+        }
+    }
 
     let path = document.createElementNS("http://www.w3.org/2000/svg", "path");
     path.setAttribute("fill", "none");
@@ -70,61 +85,80 @@ svg.onmouseup = endLine = function (e) {
     path.setAttribute("stroke-width", "5");
     path.setAttribute("stroke-linecap", "round");
 
-    if (points.length > 3) path.setAttribute("d", `M ${sx} ${sy} C ${points[1][0]} ${points[1][1]} ${points[2][0]} ${points[2][1]} ${points[3][0]} ${points[3][1]}`);
-    else path.setAttribute("d", `M ${sx} ${sy} ${ex} ${ey}`);
+    if (points.length > 3)
+        path.setAttribute("d", `M ${sx} ${sy} C ${points[1][0]} ${points[1][1]} ${points[2][0]} ${points[2][1]} ${points[3][0]} ${points[3][1]}`);
+    else
+        path.setAttribute("d", `M ${sx} ${sy} ${ex} ${ey}`);
+
     for (let i = 4; i < points.length; i++) {
-        if (i % 3 == 0 || i == points.length - 1) path.setAttribute("d", path.getAttribute("d") + ` S ${points[i - 1][0]} ${points[i - 1][1]} ${points[i][0]} ${points[i][1]}`);
+        if (i % 3 == 0 || i == points.length - 1)
+            path.setAttribute("d", path.getAttribute("d") + ` S ${points[i - 1][0]} ${points[i - 1][1]} ${points[i][0]} ${points[i][1]}`);
     }
+
     svg.appendChild(path);
     svg.removeChild(line);
-    if(points.length > 3 && (sx - ex)**2 + (sy - ey)**2 < 150**2){
-        let leastX = points[0][0], leastY = points[0][1], mostX = points[0][0], mostY = points[0][1];
-        for(let i = 0; i < points.length; i++){
-            for(let j = 0; j < points.length; j++){
-                if(mostX < points[j][0])
-                    mostX = points[j][0];
-                if(leastX > points[j][0])
-                    leastX = points[j][0];
-                if(mostY < points[j][1])
-                    mostY = points[j][1];
-                if(leastY > points[j][1])
-                    leastY = points[j][1];
+    if (points.length > 3 && (sx - ex) ** 2 + (sy - ey) ** 2 < 150 ** 2) {
+        let diff = { x: 1, y: 1 };
+        let lastDiff = { x: 1, y: 1 };
+        let endPoints = [];
+        for (let i = 1; i < points.length; i++) {
+            diff.x = points[i - 1][0] / points[i][0];
+            diff.y = points[i - 1][1] / points[i][1];
+            let RoC = { x: Math.abs((diff.x / lastDiff.x) - 1) + 1, y: Math.abs((diff.y / lastDiff.y) - 1) + 1 };
+
+            if (RoC.x * RoC.y > 1.015) {
+                endPoints.push(points[i]);
+                // console.log(RoC.x * RoC.y);
             }
+            lastDiff.x = diff.x;
+            lastDiff.y = diff.y;
+            // if(i+5 >= points.length && !(i >= points.length - 1)) i+= (points.length - i) - 1;
+            // else i+=5;
         }
-        let cx = (leastX+mostX)/2;
-        let cy = (leastY+mostY)/2;
-        let radius = (mostX-leastX)/2 >= (mostY-leastY)/2 ? (mostX-leastX)/2 : (mostY-leastY)/2;
-        let pointDist = (2*Math.PI*radius)/points.length;
-        let angle = pointDist/radius;
-        let circlePath = document.createElementNS("http://www.w3.org/2000/svg", "path");
-        circlePath.setAttribute("fill","none");
-        circlePath.setAttribute("stroke","white");
-        circlePath.setAttribute("stroke-width","5");
-        circlePath.setAttribute("stroke-linecap","round");
-        circlePath.setAttribute("d", `M ${cx + radius} ${cy} `);
-        let circlePoints = [];
-        let id = null;
-        for(let i = 0; i < points.length; i++){
-            let curAngle = angle * (i+1);
-            circlePath.setAttribute("d", circlePath.getAttribute("d") + `A ${radius} ${radius} ${curAngle} 0 1 ${cx + radius * Math.cos(curAngle)} ${cy + radius * Math.sin(curAngle)} `);
-            circlePoints.push([cx + radius * Math.cos(curAngle), cy + radius * Math.sin(curAngle)]);
+        if (endPoints.length > 2) {
+            let shape = document.createElementNS("http://www.w3.org/2000/svg", "polyline");
+            shape.setAttribute("fill", "none");
+            shape.setAttribute("stroke", "white");
+            shape.setAttribute("stroke-width", "5");
+            shape.setAttribute("stroke-linecap", "round");
+            shape.setAttribute("points", `${sx},${sy}`);
+            for (let i = 0; i < endPoints.length; i++) {
+                shape.setAttribute("points", shape.getAttribute("points") + ` ${endPoints[i][0]} ${endPoints[i][1]}`);
+            }
+            shape.setAttribute("points", shape.getAttribute("points") + ` ${sx} ${sy}`);
+            svg.appendChild(shape);
+        } else {
+            let cx = (leastX + mostX) / 2;
+            let cy = (leastY + mostY) / 2;
+            let radius = (mostX - leastX) / 2 >= (mostY - leastY) / 2 ? (mostX - leastX) / 2 : (mostY - leastY) / 2;
+            let pointDist = (2 * Math.PI * radius) / points.length;
+            let angle = pointDist / radius;
+            let circlePath = document.createElementNS("http://www.w3.org/2000/svg", "path");
+            circlePath.setAttribute("fill", "none");
+            circlePath.setAttribute("stroke", "white");
+            circlePath.setAttribute("stroke-width", "5");
+            circlePath.setAttribute("stroke-linecap", "round");
+            circlePath.setAttribute("d", `M ${cx + radius} ${cy} `);
+            let circlePoints = [];
+            for (let i = 0; i < points.length; i++) {
+                let curAngle = angle * (i + 1);
+                circlePath.setAttribute("d", circlePath.getAttribute("d") + `A ${radius} ${radius} ${curAngle} 0 1 ${cx + radius * Math.cos(curAngle)} ${cy + radius * Math.sin(curAngle)} `);
+                circlePoints.push([cx + radius * Math.cos(curAngle), cy + radius * Math.sin(curAngle)]);
+            }
+            // let frames = 20;
+            // for (let i = 0; i < frames; i++) {
+            //     id = setTimeout(async function () {
+            //         for (let j = 0; j < points.length; j++) {
+            //             // console.log(i + " " + j);
+            //             let distX = (points[j][0] + circlePoints[j][0]) / frames;
+            //             let distY = (points[j][1] + circlePoints[j][1]) / frames;
+            //             console.log("lerp");
+            //         }
+            //         clearTimeout(id);
+            //     }, 100 * i);
+            // }
+            svg.appendChild(circlePath);
         }
-        console.log(points.length);
-        console.log(circlePoints.length);
-        // let frames = 20;
-        // for(let i = 0; i < frames; i++){
-        //     id = setTimeout(async function() {
-        //         for(let j = 0; j < points.length; j++){
-        //             // console.log(i + " " + j);
-        //             let distX = (points[j][0] + circlePoints[j][0]) / frames;
-        //             let distY = (points[j][1] + circlePoints[j][1]) / frames;
-        //             console.log("lerp");
-        //         }
-        //         clearTimeout(id);
-        //     }, 100 * i);
-        // }
-        // clearTimeout(id);
-        svg.appendChild(circlePath);
         svg.removeChild(path)
     }
 }
@@ -134,8 +168,8 @@ svg.onmousemove = drawLine = function (e) {
 
     x = ((mouseX - svgRect.left) * (viewBox.width / svgRect.width)).toFixed(3);
     y = ((mouseY - svgRect.top) * (viewBox.height / svgRect.height)).toFixed(3);
-    let dist = (x - svgCenter.x)**2 + (y - svgCenter.y)**2;
-    if (dist >= (viewBox.width/2)**2) {
+    let dist = (x - svgCenter.x) ** 2 + (y - svgCenter.y) ** 2;
+    if (dist >= (viewBox.width / 2) ** 2) {
         endLine(e);
     }
 
@@ -151,9 +185,9 @@ function cullPoints(points) {
         let pos = points[j];
         let lineA = pos[0] - refPoint[0];
         let lineB = pos[1] - refPoint[1];
-        let lineC = (lineA * lineA) + (lineB * lineB);
+        let lineC = (lineA ** 2) + (lineB ** 2);
 
-        if (j != 0 && j != points.length - 1 && lineC < min * min) {
+        if (j != 0 && j != points.length - 1 && lineC < min ** 2) {
             continue;
         } else {
             refPoint = pos;
@@ -166,7 +200,7 @@ function cullPoints(points) {
     return temp;
 }
 
-function refresh(){
+function refresh() {
     svgRect = svg.getClientRects()[0];
-    svgCenter = {x: ((svgRect.width/2) * (viewBox.width/svgRect.width)).toFixed(3), y: ((svgRect.height/2) * (viewBox.height/svgRect.height)).toFixed(3)};
+    svgCenter = { x: ((svgRect.width / 2) * (viewBox.width / svgRect.width)).toFixed(3), y: ((svgRect.height / 2) * (viewBox.height / svgRect.height)).toFixed(3) };
 }
